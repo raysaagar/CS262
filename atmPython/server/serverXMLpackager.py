8821c5c3cbbf3849a68350a393a09ba1a1ae1b17
Saagar Milind Deshpande <saagar@hcs.harvard.edu>
2014-02-25 23:31:03 -0800

    checksum added to server xml

--- atmPython/server/serverXMLpackager.py
+++ atmPython/server/serverXMLpackager.py
@@ -1,4 +1,5 @@
 from lxml import etree
+from hashlib import sha384
 
 class XMLPackage(object):
 
@@ -25,7 +26,14 @@ class XMLPackage(object):
 
         without_checksum = etree.tostring(root)
 
-        # TODO: sha checksum. need sal's commit
+        # get a checksum of everything without checksum tag
+        pre_checksum = etree.tostring(root)
+        checksum = sha384(pre_checksum)
+
+        checksum_elm = etree.Element('checksum')
+        # convert binary to hex
+        checksum_elm.text = checksum.hexdigest()
+        root.append(checksum_elm)
 
         s = etree.tostring(root)
 
