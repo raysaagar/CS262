aeaf1ceff451d03a393c5416a8b266686f59fefe
Saagar Milind Deshpande <saagar@hcs.harvard.edu>
2014-02-26 21:19:17 -0800

    my server send

--- atmPython/server/myServerSend.py
+++ atmPython/server/myServerSend.py
@@ -5,6 +5,9 @@ Altered Feb 20, 2014
 '''
 
 from struct import pack
+from serverXMLpackager import XMLPackage as xml
+
+version = '0.0.1'
 
 def general_failure(conn, type, reason):
 
@@ -21,44 +24,53 @@ def general_failure(conn, type, reason):
         typebyte = '\x52'
 
     #encode and send the string
-    utf = reason.encode('utf-8')
-    utflen = len(utf)
-    conn.send('\x01' + pack('!I',2 + utflen) + typebyte + pack('!h',utflen) + utf)
+    #utf = reason.encode('utf-8')
+    #utflen = len(utf)
+    #conn.send('\x01' + pack('!I',2 + utflen) + typebyte + pack('!h',utflen) + utf)
+    conn.send(version, typebyte, reason.encode('utf-8'), True)
     return
 
 #create new account
 def create_success(conn,act):
-    conn.send('\x01' + pack('!I',4) +'\x11' + pack('!I',act))
+    #xml = server_package(version, 4, act, False)
+    #conn.send('\x01' + pack('!I',4) +'\x11' + pack('!I',act))
+    conn.send(server_package(version, '\x11', act, False)
     return
 
 #delete an existing account
 def delete_success(conn):
-    conn.send('\x01\x00\x00\x00\x00\x21')
+    #conn.send('\x01\x00\x00\x00\x00\x21')
+    conn.send(server_package(version, '\x21', "account deleted", False))
     return
 
 #deposit to an existing account
 def deposit_success(conn,bal):
-    conn.send('\x01' + pack('!I',4) +'\x31' + pack('!I',bal))
+    #conn.send('\x01' + pack('!I',4) +'\x31' + pack('!I',bal))
+    conn.send(server_package(version, '\x31', bal, False)
     return
 
 #withdraw from an existing account
 def withdraw_success(conn,bal):
-    conn.send('\x01' + pack('!I',4) +'\x41' + pack('!I',bal))
+    #conn.send('\x01' + pack('!I',4) +'\x41' + pack('!I',bal))
+    conn.send(server_package(version, '\x41', bal, False)
     return
 
 #withdraw from an existing account
 def balance_success(conn,bal):
-    conn.send('\x01' + pack('!I',4) +'\x51' + pack('!I',bal))
+    #conn.send('\x01' + pack('!I',4) +'\x51' + pack('!I',bal))
+    conn.send(server_package(version, '\x51', bal, False)
     return
 
 #end a session
 def end_session_success(conn):
-    conn.send('\x01\x00\x00\x00\x00\x61')
+    #conn.send('\x01\x00\x00\x00\x00\x61')
+    conn.send(server_package(version, '\x61', bal, False)
     return
 
 #handle invalid opcodes
 def unknown_opcode(conn):
-    conn.send('\x01\x00\x00\x00\x00\x62')
+    #conn.send('\x01\x00\x00\x00\x00\x62')
+    conn.send(server_package(version, '\x61', "unknown opcode", False)
     return
 
 
