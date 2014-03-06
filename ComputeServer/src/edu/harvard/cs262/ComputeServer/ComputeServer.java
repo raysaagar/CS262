5f5eaa49551e2d87f3a4b81c9123410bdcfe1b67
Jim Waldo <jiniwaldo@gmail.com>
2014-03-06 12:07:46 -0500

    Added some documentation

--- ComputeServer/src/edu/harvard/cs262/ComputeServer/ComputeServer.java
+++ ComputeServer/src/edu/harvard/cs262/ComputeServer/ComputeServer.java
@@ -4,16 +4,25 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 /**
- * The {@code Remote} interface for a compute server
- * </p>
+ * The {@code Remote} interface for a compute server </p>
  * 
  * This interface defines a remote server that will receive work from client,
- * and return an {@code Object} that encapsulates the results of the work. 
- * @author waldo
- *
+ * and return an {@code Object} that encapsulates the results of the work.
+ * 
+ * 
  */
-public interface ComputeServer extends Remote{
+public interface ComputeServer extends Remote {
+	/**
+	 * Get a {@link WorkTask} object, which will be performed
+	 * 
+	 * @param work
+	 *            a {@link WorkTask} object, essentially a {@link Serializeable}
+	 *            object that encapsulates a closure of some work that needs to
+	 *            be done
+	 * @return an {@link Object} that encapsulates the result of the work that
+	 *         has been performed
+	 * @throws RemoteException
+	 */
 	public Object sendWork(WorkTask work) throws RemoteException;
-	
 
 }
--- ComputeServer/src/edu/harvard/cs262/ComputeServer/WorkQueue.java
+++ ComputeServer/src/edu/harvard/cs262/ComputeServer/WorkQueue.java
@@ -8,16 +8,36 @@ import java.rmi.RemoteException;
 import java.util.UUID;
 
 /**
- * A WorkQueue is a server that allows other {@code ComputeServer} to register with it, and then hands out the 
- * work to all the registered servers</p>
+ * A WorkQueue is a server that allows other {@code ComputeServer} to register
+ * with it, and then hands out the work to all the registered servers</p>
  * 
- * This type of server may not do any computation itself, but will allow elastic scaling of the compute server
- * infrastructure by allowing others to register and do work.</p>
- *
+ * This type of server may not do any computation itself, but will allow elastic
+ * scaling of the compute server infrastructure by allowing others to register
+ * and do work.</p>
+ * 
  */
-public interface WorkQueue extends Remote{
-	public UUID registerWorker(ComputeServer server)
-		throws RemoteException;
-	public boolean unregisterWorker(UUID workerID)
-		throws RemoteException;
+public interface WorkQueue extends Remote {
+	/**
+	 * Register a {@link ComputerServer} with the work queue, allowing that
+	 * server to be assigned {@link WorkTask} objects to perform.
+	 * 
+	 * @param server
+	 *            the {@link ComputeServer} to be added to the pool of workers
+	 * @return the {@link UUID} generated to identify the worker; this will be
+	 *         used in calls to {@link unregisterWorker}
+	 * @throws RemoteException
+	 */
+	public UUID registerWorker(ComputeServer server) throws RemoteException;
+
+	/**
+	 * Unregister a {@link ComputeServer} from the pool of workers
+	 * 
+	 * @param workerID
+	 *            the {@link UUID} that identifies the server to be removed from
+	 *            the pool; this was returned by {@link registerWorker} when the
+	 *            server was added to the pool
+	 * @return {@code true} if the removal was successful; {@code false} if not
+	 * @throws RemoteException
+	 */
+	public boolean unregisterWorker(UUID workerID) throws RemoteException;
 }
