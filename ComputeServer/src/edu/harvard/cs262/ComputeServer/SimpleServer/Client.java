bada6828bb605aa7caf3a162727bcdd7c20d3920
Saagar Deshpande <raysaagar@gmail.com>
2014-03-13 16:30:02 -0400

    some fixes, client is ready with params

--- ComputeServer/src/edu/harvard/cs262/ComputeServer/SimpleServer/Client.java
+++ ComputeServer/src/edu/harvard/cs262/ComputeServer/SimpleServer/Client.java
@@ -15,29 +15,38 @@ public class Client {
 	 */
 	
 	public static void main(String[] args) {
+        String hostname = args[0];
+//		int port = Integer.parseInt(args[1]);
         int port = 1099;
 
 		// TODO Auto-generated method stub
-            System.setProperty("java.security.policy", "security.policy");
+        System.setProperty("java.security.policy", "security.policy");
 
-			if (System.getSecurityManager()==null){
-				System.setSecurityManager(new SecurityManager());
-			}
+		if (System.getSecurityManager()==null){
+			System.setSecurityManager(new SecurityManager());
+		}
 
-		 if (System.getSecurityManager() == null) {
-	            System.setSecurityManager(new SecurityManager());
-	        }
 		 try {
-	            String name = "SimpleServer";
-	            Registry registry = LocateRegistry.getRegistry(args[0], port);
-	            ComputeServer comp = (ComputeServer) registry.lookup(name);
-	            WorkTask work = new HelloTask("Hello");
-	            String response = (String) comp.sendWork(work);
-	            System.out.println(response);
-	        } catch (Exception e) {
-	            System.err.println("Client exception: " + e.toString());
-	            e.printStackTrace();
-	        }
+            String name = "SimpleServer";
+            // hack to force compile
+            Registry registry = null;
+            try {
+            	System.out.println("Establishing connection to server " + hostname + ":" + port);
+            	registry = LocateRegistry.getRegistry(hostname, port);
+            }
+            catch (Exception e) {
+            	System.out.println("Unable to connect to server " + hostname + ":" + port);
+            	System.out.println(e);
+            }
+            
+            ComputeServer comp = (ComputeServer) registry.lookup(name);
+            WorkTask work = new HelloTask("Hello");
+            String response = (String) comp.sendWork(work);
+            System.out.println(response);
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
 		 
 	}
 
--- ComputeServer/src/tasks/HelloTask.java
+++ ComputeServer/src/tasks/HelloTask.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 public class HelloTask implements WorkTask, Serializable {
 
     String str;
+	private static final long serialVersionUID = 1L;
 
     public HelloTask(String str) {
         this.str = str;
